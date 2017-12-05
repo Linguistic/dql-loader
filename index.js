319a2397f3bec9e302ef8c722d848f5b9ce0bffa
Tyler Nickerson <nickersoft@gmail.com>
2017-12-05 13:17:46 -0500

    Modified to use parser + added IntelliJ support

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const se = require('safe-eval');
 const utils = require('loader-utils');
+const parser = require('dql').parser;
 
 module.exports = function(source) {
     this.cacheable && this.cacheable();
     const options = utils.getOptions(this);
     const flavor = options && options.flavor ? options.flavor : 'mysql';
-    return `module.exports = (require('dql').${flavor})\`${se(source, {
-        module
-    }).replace(/\s+/g, ' ').trim()}\``;
+    const parsed = parser.parse(se(source, { module }));
+    return `module.exports = (require('dql').${flavor})(${JSON.stringify(parsed)})`;
 };
